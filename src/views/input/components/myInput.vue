7585853518c81df8df811666b13b2840b9939ee2
s2kzzZ <1428851833@qq.com>
2023-04-07 13:58:20 +0800

    add

---
+++ src/views/input/components/myInput.vue
@@ -0,0 +1,42 @@
+<!-- 
+  组件封装
+  1.通过userAttrs()直接获取父组件传来的内容，无需分别单独写
+  2.父组件传递几个插槽就是使用传递的几个
+  3.父组件通过ref调用子组件ref上的方法
+ -->
+<template>
+  <div class="my-input">
+    <el-input ref="inp" v-bind="attrs">
+      <template v-for="(value, name) in slots" #[name]="slotData">
+        <slot :name="name" v-bind="slotData || {}"></slot>
+      </template>
+    </el-input>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, useAttrs, useSlots } from "vue";
+const attrs = useAttrs();
+const slots = useSlots();
+const inp = ref(null);
+const defineExposeObj: any = {}; // ref上的所有方法
+onMounted(() => {
+  // console.log("inp", inp.value);
+  const entries = Object.entries(inp.value as any);
+  for (const [key, value] of entries) {
+    defineExposeObj[key] = value;
+  }
+  // console.log(defineExposeObj);
+});
+defineExpose(defineExposeObj);
+</script>
+
+<style scoped lang="scss">
+.my-input {
+  transition: 0.3s;
+  &:hover,
+  &:focus-within {
+    filter: drop-shadow(0 0 3px rgba(0, 0, 0, 0.3));
+  }
+}
+</style>
---
+++ src/views/input/input.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <myInput ref="inputRef" v-model="data" :placeholder="placeholder">
+      <template #prepend>
+        <el-select
+          v-model="selectValue"
+          placeholder="请选择"
+          style="width: 120px"
+        >
+          <el-option lable="a" value="option1"></el-option>
+          <el-option lable="b" value="option2"></el-option>
+          <el-option lable="c" value="option3"></el-option>
+        </el-select>
+      </template>
+    </myInput>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, nextTick } from "vue";
+import myInput from "./components/myInput.vue";
+const data = ref("");
+const placeholder = ref("请输入");
+const selectValue = ref("");
+const inputRef = ref<any>(null);
+nextTick(() => {
+  // console.log(inputRef.value);
+  inputRef.value?.focus();
+});
+</script>
